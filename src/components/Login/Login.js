037cfe39a4b4e605e76d19203f7cfa8573ba1fed
James Zuliani <zulianijames@gmail.com>
2023-05-12 15:54:01 -0400

    login and signup component complete

---
+++ src/components/Login/Login.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const history = useHistory();
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    axios
+      .post("api/auth/login", { username, password })
+      .then((res) => {
+        localStorage.setItem("token", res.data.token);
+        history.push("/");
+      })
+      .catch((err) => setError(err.response.data.error));
+  }
+
+  return (
+    <form onSubmit={handleLogin}>
+      <h1>Login</h1>
+      {error && <p>{error}</p>}
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
