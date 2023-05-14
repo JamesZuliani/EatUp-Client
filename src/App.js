9291ead300c05e1c2ecb658e8b2159b936d76d60
James Zuliani <zulianijames@gmail.com>
2023-05-14 00:46:33 -0400

    Saved Meals page functionally complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from "react";
+import React, {useState} from "react";
+
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
-import "./App.scss";
 import RecipePage from "./pages/RecipePage/RecipePage";
+import SavedMealsPage from "./pages/SavedMealsPage/SavedMealsPage";
+import "./App.scss";
+
+
 
 function App() {
+
+  const [savedMeals, setSavedMeals] = useState([]);
+
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/recipes" element = {<RecipePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/saved-meals" element={<SavedMealsPage savedMeals={savedMeals} setSavedMeals={setSavedMeals} />} />
+        <Route path="/recipes" element={<RecipePage />} />
+        {/* <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/signup" element={<Signup />} /> */}
       </Routes>
     </BrowserRouter>
   );
